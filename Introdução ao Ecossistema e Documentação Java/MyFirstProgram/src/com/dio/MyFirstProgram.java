de225d77ddc186cdecbca822fe363b837e6499bb
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-01-16 16:59:27 -0300

    Estrutura de condição if and switch

--- Introdução ao Ecossistema e Documentação Java/MyFirstProgram/src/com/dio/MyFirstProgram.java
+++ Introdução ao Ecossistema e Documentação Java/MyFirstProgram/src/com/dio/MyFirstProgram.java
@@ -1,12 +1,16 @@
 package com.dio;
 
 import com.dio.base.Order;
+import com.dio.base.Value;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
 
         Order order = new Order("1234");
         System.out.println(order);
+
+        Value totalValue = new Value(99);
+        System.out.println(totalValue);
 
     }
 }
