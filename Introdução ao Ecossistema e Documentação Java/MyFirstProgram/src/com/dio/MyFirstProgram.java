2d262fb5088bc5c1e06c9e66db0ad5868fe624a8
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-01-16 23:18:40 -0300

    Estrutura de Repetição

--- Introdução ao Ecossistema e Documentação Java/MyFirstProgram/src/com/dio/MyFirstProgram.java
+++ Introdução ao Ecossistema e Documentação Java/MyFirstProgram/src/com/dio/MyFirstProgram.java
@@ -1,16 +1,12 @@
 package com.dio;
 
 import com.dio.base.Order;
-import com.dio.base.Value;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
 
-        Order order = new Order("1234");
+        Order order = new Order("1234", 99);
         System.out.println(order);
-
-        Value totalValue = new Value(99);
-        System.out.println(totalValue);
 
     }
 }
