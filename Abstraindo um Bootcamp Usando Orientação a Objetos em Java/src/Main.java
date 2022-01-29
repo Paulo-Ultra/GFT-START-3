c1e9be70c4735042a34774a78fb611f3b4e22df8
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-01-29 16:17:06 -0300

    Abstração e Encapsulamento - Curso e Mentoria

---
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/src/Main.java
@@ -0,0 +1,31 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("decrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("decrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
+}
